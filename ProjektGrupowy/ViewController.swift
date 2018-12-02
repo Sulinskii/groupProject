8d56707cc9fed36de5efde45fba8e394fe552ee0
Artur Sulinski <artur.sulinski@brightinventions.pl>
2018-12-02 17:02:18 +0100

    Initial Commit

---
+++ ProjektGrupowy/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ProjektGrupowy
+//
+//  Created by Artur Sulinski on 02/12/2018.
+//  Copyright © 2018 Artur Sulinski. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
